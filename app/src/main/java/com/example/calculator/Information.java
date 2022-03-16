580c89734652244ea7cefbda2c53e8c6ce0a6f7a
Oybek <Oybek22.04.2002@mail.ru>
2022-03-17 00:16:54 +0300

    Calculator_v2(with trash)

--- app/src/main/java/com/example/calculator/Information.java
+++ app/src/main/java/com/example/calculator/Information.java
@@ -1,17 +1,73 @@
 package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Information extends AppCompatActivity {
 
     private static final String TAG = "myLogs";
+    private Toolbar toolbar_info;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        toolbar_info = (Toolbar) findViewById(R.id.toolbar);
+        toolBarSet();
+
+        ImageButton imageButton = (ImageButton) findViewById(R.id.my_github);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.my_github)));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Log.d(TAG, "Не получается обработать намерение запуска github!");
+                }
+            }
+        });
+
+        imageButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Кнопку надо отпускать :)", Toast.LENGTH_SHORT);
+                toast.show();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.my_github)));
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    Log.d(TAG, "Не получается обработать намерение запуска github!");
+                }
+                return false;
+            }
+        });
+
+    }
+
+    private void toolBarSet() {
+        setSupportActionBar(toolbar_info);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        getSupportActionBar().setTitle("Информация");
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
     @Override
--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -16,6 +19,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.appcompat.widget.Toolbar; // вроде как является современной android.support.v7.widget.Toolbar(но это не точно, в случае чего в java и xml заменить на Toolbar
+
 import org.w3c.dom.EntityReference;
 
 import java.io.IOException;
@@ -23,17 +28,23 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "myLogs";
+    private Toolbar toolbar_calc;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radios);
-        TextView res = findViewById(R.id.res);
-        EditText text = findViewById(R.id.value);
-        EditText base = findViewById(R.id.base);
-        ToggleButton toggleButton = findViewById(R.id.toggleButton);
-        ImageButton imageButton = findViewById(R.id.info);
+        TextView res = (TextView) findViewById(R.id.res);
+        EditText text = (EditText) findViewById(R.id.value);
+        EditText base = (EditText) findViewById(R.id.base);
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+//        ImageButton imageButton = findViewById(R.id.info);
+        toolbar_calc = (Toolbar) findViewById(R.id.toolbar_calc);
+        setSupportActionBar(toolbar_calc);
+        getSupportActionBar().setTitle("Калькулятор");
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -177,31 +188,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+//        imageButton.setOnClickListener(new View.OnClickListener() {     Удалить!!!
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent("android.intent.action.Information");
+//                //intent.setAction("android.intent.action.Information");
+//                if (intent.resolveActivity(getPackageManager()) != null) {
+//                    startActivity(intent);
+//                } else {
+//                    Log.d(TAG, "Не получается обработать намерение!");
+//                }
+//            }
+//        });
+//        imageButton.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View view) {
+//                Toast toast = Toast.makeText(getApplicationContext(), "Кнопку надо отпускать :)", Toast.LENGTH_SHORT);
+//                toast.show();
+//                Intent intent = new Intent();
+//                intent.setAction("android.intent.action.Information");
+//                startActivity(intent);
+//                return false;
+//            }
+//        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.info_menu, menu);
+        return true; // вернуть true? super.onCreateOptionsMenu(menu)
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.info:
                 Intent intent = new Intent("android.intent.action.Information");
-                //intent.setAction("android.intent.action.Information");
+                intent.setAction("android.intent.action.Information");
                 if (intent.resolveActivity(getPackageManager()) != null) {
                     startActivity(intent);
-                } else {
+                } else
                     Log.d(TAG, "Не получается обработать намерение!");
-                }
-            }
-        });
-        imageButton.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Кнопку надо отпускать :)", Toast.LENGTH_SHORT);
-                toast.show();
-                Intent intent = new Intent();
-                intent.setAction("android.intent.action.Information");
-                startActivity(intent);
-                return false;
-            }
-        });
-
-
+        }
+        return true;
     }
 
     @Override
@@ -210,3 +241,4 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "MainActivity: onStart");
     }
 }
+// toolbar.setSubtitle("Test Subtitle");
